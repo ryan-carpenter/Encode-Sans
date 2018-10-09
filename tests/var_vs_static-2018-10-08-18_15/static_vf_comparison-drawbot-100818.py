6dbccf8a021976e17a4410ccc5abf8772621b6c4
Stephen Nixon <swr410@gmail.com>
2018-10-09 15:12:52 -0400

    update drawbot test

--- tests/var_vs_static-2018-10-08-18_15/static_vf_comparison-drawbot-100818.py
+++ tests/var_vs_static-2018-10-08-18_15/static_vf_comparison-drawbot-100818.py
@@ -1,36 +1,55 @@
-import os
-rect(0,0,1000,1000)
+W,H = 860*2, 340*2
 
-testString="Lorem \nTestem \n845"
+resolution = 2
 
+newPage(W,H)
 
-font("../google-fonts-static/Encode_Sans/EncodeSans-Regular.ttf", 250)
-# # also tried using OS tools to see if it might help
-# os.chdir("..")
-# font("google-fonts-static/Encode_Sans/EncodeSans-Regular.ttf", 250)
+rect(0,0,W,H)
 
-# sometimes an error says LucidaGrande is subbing in, but this looks different...
-# font("Ludida Grande", 250) 
+testString = "In statistical modeling, regression analysis is a set of statistical processes for estimating the relationships among variables. It includes many techniques for modeling and analyzing several variables, when the focus is on the relationship between a dependent variable and one or more independent variables (or 'predictors'). More specifically, regression analysis helps one understand how the typical value of the dependent variable (or 'criterion variable') changes when any one of the independent variables is varied, while the other independent variables are held fixed. \n \nMost commonly, regression analysis estimates the conditional expectation of the dependent variable given the independent variables – that is, the average value of the dependent variable when the independent variables are fixed. Less commonly, the focus is on a quantile, or other location parameter of the conditional distribution of the dependent variable given the independent variables. In all cases, a function of the independent variables called the regression function is to be estimated. In regression analysis, it is also of interest to characterize the variation of the dependent variable around the prediction of the regression function using a probability distribution. A related but distinct approach is Necessary Condition Analysis[1] (NCA), which estimates the maximum (rather than average) value of the dependent variable for a given value of the independent variable (ceiling line rather than central line) in order to identify what value of the independent variable is necessary but not sufficient for a given value of the dependent variable."
+setLineHeight = 18*resolution
+setFontSize = 16*resolution
+# xPos, yPos = 80, 664
+xPos, yPos = W/20,H/20 
+txtW, txtH =  W-(W/10),H-(H/10)
 
-# fill(1,0,1)
-fill(1)
-text(testString, (100, 700))
+testFont = "quickfix"
+varWght = 363.63 # adjusted weight needed for "quick fix" 400.0 #
 
-font("EncodeSans-mathfix-normal_wdth-VF.ttf", 250)
+lineHeight(setLineHeight)
 
-# font("var_vs_static-2018-10-08-18_15/EncodeSans-mathfix-normal_wdth-VF.ttf", 250)
+# linking a specific font file for testing
+font("../google-fonts-static/Encode_Sans/EncodeSans-Regular.ttf", setFontSize)
 
-# font("RecursiveSans-GothicA", 250) # testing another varfont on my system
+fill(1,0,1)
+ 
+textBox(testString, (xPos, yPos, txtW, txtH))
+
+# Linking a specific font file for testing. The text() method fails for this font, even though the font works as expected in FontView and on Axis-Praxis.org
+font(f"./EncodeSans-{testFont}-normal_wdth-VF.ttf", setFontSize)
+
+# # It *does* work if I link to another variable font, such as Avenir Next Variable.
+# font("AvenirNext_Variable.ttf", setFontSize)
+
 
 for axis, data in listFontVariations().items():
     print((axis, data))
 
-fontVariations(wght=400.0,wdth=500.0)
+# when I set the fontVariations, the following text() method fails with NSInvalidArgumentException
+fontVariations(wght=varWght,wdth=500.0)
 
-# # fontVariations(wdth=500.1, wght=400.1)
+blendMode("screen")
+fill(0,1,0)
 
-# # fontVariations(wght=600.0)
+# I recieve a NSInvalidArgumentException here, which Apple describes as "Name of an exception that occurs when you pass an invalid argument to a method, such as a nil pointer where a non-nil object is required." However, I only receive this error when I set the font variations of the above variable font. So, the arguments should be valid.
+# text(testString, (xPos, yPos))
+textBox(testString, (xPos, yPos, txtW, txtH))
 
-# fill(0,.75,1)
-fill(0.075)
-text(testString, (100, 700))
+rotate(90)
+fontSize(12*resolution)
+fill(0.8)
+tracking(1)
+
+text(f"{testFont.upper()}, {'wght:'.upper()} {varWght}", (H-(H/17), -(W/32)), align="right")
+
+saveImage(f"image-exports/encode-diff_test-{testFont}-wght_{varWght}-2018_10_09-14_28.png")
