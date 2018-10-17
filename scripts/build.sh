2e5e25f462172b3f2e8b7cc03738a8a4888dc5a4
Stephen Nixon <swr410@gmail.com>
2018-10-17 09:12:45 -0400

    clean and check build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,37 +1,48 @@
-### based on build script for Encode-Sans-VF, by Mike LaGuttuta
+### WIP macOS build script for Encode Sans VF, based on a build script by Mike LaGuttuta
+### Run in the terminal by entering this file path
 ### requires a python 2 environment, for now
 
-
-###### set vars ######
+############################################
+################# set vars #################
 
 glyphsSource="sources/Encode-Sans.glyphs"
 
+## Set this. It's probably your font name without spaces, then "-VF"
+VFname="EncodeSans-VF"
+
+## if the Glyphs source has a non-rectangular master/instance arrangement, this fixes it (WIP)
+fixGlyphsDesignspace=true
+
+## move VF into new folder of dist/ with timestamp and fontbake
 timestampAndFontbakeInDist=true
 
+## keep designspace file if you want to check values later
 keepDesignspace=true
 
-###### set vars ######
+################# set vars #################
+############################################
 
 ## make temp glyphs filename with "-build" suffix
 tempGlyphsSource=${glyphsSource/".glyphs"/"-Build.glyphs"}
 
-## ttfVFName=${glyphsSource/".glyphs"/"-VF.ttf"} # can't quite do this very sensibly without python
+## TODO: grab name from Glyphs source via python & glyphsLib?
 
 ## copy Glyphs file into temp file
 cp $glyphsSource $tempGlyphsSource
 
-## call the designspace fixing script
-python2 scripts/fix-designspace.py $tempGlyphsSource
-
+if [ $fixGlyphsDesignspace == true ]
+then
+    ## call the designspace fixing script
+    python2 scripts/fix-designspace.py $tempGlyphsSource
+else
+    echo "not morphing designspace"
+fi
 
 ## call fontmake to make a varfont
 fontmake -o variable -g $tempGlyphsSource
 
-## keep designspace file if you want to look at values later
 if [ $keepDesignspace == true ]
 then
-    ## move font into dist, with timestamp – probably with a python script and datetime
-    ## and fontbake the font
     echo "designspace in master_ufo folder"
 else
     rm -rf master_ufo
@@ -43,26 +54,27 @@ rm -rf $tempGlyphsSource
 cd variable_ttf
 
 ## fix file metadata with gftools
-gftools fix-nonhinting EncodeSans-VF.ttf EncodeSans-VF.ttf
-gftools fix-dsig --autofix EncodeSans-VF.ttf
-gftools fix-gasp EncodeSans-VF.ttf
+gftools fix-nonhinting ${VFname}.ttf ${VFname}.ttf
+gftools fix-dsig --autofix ${VFname}.ttf
+gftools fix-gasp ${VFname}.ttf
 
 ## sets up temp ttx file to insert correct values into tables
-ttx EncodeSans-VF.ttf
+ttx ${VFname}.ttf
 
-rm -rf EncodeSans-VF.ttf
-rm -rf EncodeSans-VF-backup-fonttools-prep-gasp.ttf
+rm -rf ${VFname}.ttf
+rm -rf ${VFname}-backup-fonttools-prep-gasp.ttf
 
 cd ..
 
-ttxPath="variable_ttf/EncodeSans-VF.ttx"
+ttxPath="variable_ttf/${VFname}.ttx"
 
 
-## inserts patch files into new temp naming ttx
-cat $ttxPath | tr '\n' '\r' | sed -e "s~<name>.*<\/name>~$(cat scripts/NAMEpatch.xml | tr '\n' '\r')~" | tr '\r' '\n' > variable_ttf/EncodeSans-VF-name.ttx
-cat variable_ttf/EncodeSans-VF-name.ttx | tr '\n' '\r' | sed -e "s,<STAT>.*<\/STAT>,$(cat scripts/STATpatch.xml | tr '\n' '\r')," | tr '\r' '\n' > $ttxPath
+## inserts patch files into temporary ttx to fix export errors
+## BE SURE to update these patches for the real values in a given typeface
+cat $ttxPath | tr '\n' '\r' | sed -e "s~<name>.*<\/name>~$(cat scripts/NAMEpatch.xml | tr '\n' '\r')~" | tr '\r' '\n' > variable_ttf/${VFname}-name.ttx
+cat variable_ttf/${VFname}-name.ttx | tr '\n' '\r' | sed -e "s,<STAT>.*<\/STAT>,$(cat scripts/STATpatch.xml | tr '\n' '\r')," | tr '\r' '\n' > $ttxPath
 
-rm -rf variable_ttf/EncodeSans-VF-name.ttx
+rm -rf variable_ttf/${VFname}-name.ttx
 
 ## copies temp ttx file back into a new ttf file
 ttx $ttxPath
@@ -72,11 +84,13 @@ rm -rf $ttxPath
 
 ttfPath=${ttxPath/".ttx"/".ttf"}
 
+# open VF in default program; hopefully you have FontView
+open ${ttfPath}
+
 ## if you set timestampAndFontbakeInDist variable to true, this creates a new folder in 'dist' to put it into and run fontbake on
 if [ $timestampAndFontbakeInDist == true ]
 then
-    ## move font into dist, with timestamp – probably with a python script and datetime
-    ## and fontbake the font
+    ## move font into folder of dist/, with timestamp, then fontbake the font
     python3 scripts/distdate-and-fontbake.py $ttfPath
     rm -rf variable_ttf
 else
