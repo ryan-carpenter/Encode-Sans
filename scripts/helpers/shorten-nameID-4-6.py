97ca17e4d4fc8bd017d19b863133bdde2ee53aa8
Stephen Nixon <swr410@gmail.com>
2018-11-24 21:35:43 -0500

    making python file fix and rename

--- scripts/helpers/shorten-nameID-4-6.py
+++ scripts/helpers/shorten-nameID-4-6.py
@@ -26,42 +26,73 @@ abbreviations = {
 }
 
 
-# xml ed -u "//*/namerecord[@nameID='4']" -v "
-#       ${updatedNameID4}
-#     " ${path} > ${path}
 
 # xml ed -u "//*/namerecord[@nameID='6']" -v "
 #       ${updatedNameID6}
 #     " ${path} > ${path}
 
 def abbreviate(name):
-    print(name)
+    # print(name)
+    name = name.replace("b","")
+    name = name.replace("'","")
 
     for key in abbreviations.keys():
         if key in name:
             name = name.replace(key, abbreviations[key])
 
-    print(name)
+    # print(name)
 
-# check if path is file or dir
+    name = name.replace("\\n","")
 
-if os.path.isfile(path):
-    print("is file")
+    return(name)
 
-    command = 'xml sel -t -v "//*/namerecord[@nameID=\'4\']" ' + path
+def ttxAndFix(path):
 
+    # make temp ttx of input file
+    command = "ttx " + path
+    print(subprocess.check_output(command, shell=True))
+    tmpPath = path.replace(".ttf",".ttx")
+
+    # make command to select nameID 4, then abbreviate it and make it a variable
+    command = 'xml sel -t -v "//*/namerecord[@nameID=\'4\']" ' + tmpPath
     output = str(subprocess.check_output(command, shell=True))
+    newName4 = abbreviate(output)
 
-    abbreviate(output)
+    # repeat for nameID 6
+    command = 'xml sel -t -v "//*/namerecord[@nameID=\'6\']" ' + tmpPath
+    output = str(subprocess.check_output(command, shell=True))
+    newName6 = abbreviate(output)
 
-    # print(output)
+    print(newName4,newName6)
 
-    # for key in abbreviations.keys():
-    #     if key in output:
-    #         print(key, abbreviations[key])
-    #         output = output.replace(key, abbreviations[key])
+    tmpPath1 = tmpPath.replace(".ttx","-fix.ttx")
 
-    # print(output)
+    # insert new names 4 into ttx
+    command = "xml ed -u '//*/namerecord[@nameID=\"4\"]' -v '" + newName4 + "' " + tmpPath + " > " + tmpPath1
+    print(subprocess.check_output(command, shell=True))
+
+    tmpPath2 = tmpPath1.replace(".ttx","-fix.ttx")
+
+    # insert new name 6 into ttx
+    command = "xml ed -u '//*/namerecord[@nameID=\"6\"]' -v '" + newName6 + "' " + tmpPath + " > " + tmpPath2
+    print(subprocess.check_output(command, shell=True))
+
+    # make tmpPath back into font file
+    command = "ttx " + tmpPath2
+    print(subprocess.check_output(command, shell=True))
+
+    os.remove(path)
+    os.remove(tmpPath)
+    os.remove(tmpPath1)
+    os.remove(tmpPath2)
+
+    os.rename(path.replace(".ttf","-fix-fix.ttf"),path)
+
+# check if path is file
+if os.path.isfile(path):
+    ttxAndFix(path)
+
+
 
 # if file:
     # do function to edit name id 4 and 6
