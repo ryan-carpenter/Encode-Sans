1f86d5e4000c468e8a0075531e7f1fed5d051f84
Stephen Nixon <swr410@gmail.com>
2019-03-19 13:45:49 -0400

    finish (?) script to split and push families for separate PRs

--- sources/scripts/split-for-gf-pr.sh
+++ sources/scripts/split-for-gf-pr.sh
@@ -32,10 +32,15 @@ if [[ $subfamilyDir != *"sc" ]]; then #exclude SC versions, per Marc's request
     # navigate to google/fonts repo, then make PR branch ----------------
 
     cd $gFontsDir
+    # make sure we are up-to-date with upstream master
     git checkout master
     git pull upstream master
-    git checkout -B $subfamilyName-vf
     git reset --hard
+    # checkout fresh branch
+    git checkout -B $subfamilyName-vf
+    # clear out any lingering, untracked files
+    git reset --hard
+    git clean -f -d
 
     # -------------------------------------------------------------------
     # cleanup existing files --------------------------------------------
@@ -49,22 +54,37 @@ if [[ $subfamilyDir != *"sc" ]]; then #exclude SC versions, per Marc's request
 
     # -------------------------------------------------------------------
     # move fonts --------------------------------------------------------
-    mkdir -p ofl/$(basename $subfamilyDir)
+    mkdir -p ofl/$subfamilyName
 
     # copy font in split_vf to the branch
-    splitVF=$(ls $encodeDir/$subfamilyDir/split_vf/*.ttf) # should only be one
+    splitVF=$(ls $encodeDir/$subfamilyDir/split_vf/*.ttf) # should be only one file
     cp $splitVF ofl/$subfamilyName/$(basename $splitVF)
 
+    # copy description
+    cp $encodeDir/$subfamilyDir/DESCRIPTION.en_us.html ofl/$subfamilyName/DESCRIPTION.en_us.html
+
+    # make new Metadata
+    gftools add-font ofl/$subfamilyName
+
     # copy in statics to "static" folder
+    mkdir -p ofl/$subfamilyName/static
+    staticsToMove=$(ls $encodeDir/$subfamilyDir/static/*.ttf)
+    for static in $staticsToMove; do
+        cp $static ofl/$subfamilyName/static/$(basename $static)
+    done
 
-    
+    # -------------------------------------------------------------------
+    # commit files ------------------------------------------------------
 
-    # push to upstream branch to make PR simple
-    # TODO: ¿Make a PR from the command line?
+    git add .
+    git commit -m "add fresh split VF and statics for ofl/$subfamilyName"
+
+    # push to upstream branch (you must manually go to GitHub to make PR from there)
+    git push --force origin $subfamilyName-vf
 
     # reset as needed
     cd $encodeDir
-    # git checkout master
+    git checkout master
 fi
 done
 
