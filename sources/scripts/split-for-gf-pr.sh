7cf5fedf210b49e0c49e074e6962a70e4553ab6e
Stephen Nixon <swr410@gmail.com>
2019-03-19 12:40:18 -0400

    working out copy script

--- sources/scripts/split-for-gf-pr.sh
+++ sources/scripts/split-for-gf-pr.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
 
-# set -e
+# This script copies the latest builds to the google fonts dir in order to run QA checks and prep for a PR
+#
+# USAGE: 
+# call this script from the root of your encode repo, with the absolute path your your google/fonts repo:
+# `move-check <your_username>/<path>/fonts`
 
-# for each family:
-# grab files in fonts folder
-# move to local google/fonts repo, create branch, add copy files
+set -e
+source venv/bin/activate
 
-# temp: sort statics into static folders
+gFontsDir=$1
+
+if [[ -z "$gFontsDir" || $gFontsDir = "--help" ]] ; then
+    echo 'Add absolute path to your Google Fonts Git directory, like:'
+    echo 'move-check /Users/username/type-repos/google-font-repos/fonts'
+    exit 2
+fi
 
 fontDirs=$(ls -d fonts/*)
 
-for dir in $fontDirs; do
-    statics=$(ls $dir/*.ttf)
-    echo $dir
-    # echo $statics
-    mkdir -p $dir/static
-    git mv $dir/fontbakery-checks $dir/static/fontbakery-checks
-    for static in $statics; do
-        echo $static
-        echo $(basename $static)
-        git mv $static $dir/static/$(basename $static)
-    done
-    echo ========================
+encodeDir=$(pwd)
+encodeQADir=$encodeDir/misc/googlefonts-qa
+
+for subfamilyDir in $fontDirs; do
+
+    subfamilyName=$(basename $dir)
+    # -------------------------------------------------------------------
+    # navigate to google/fonts repo, then make PR branch ----------------
+
+    cd $gFontsDir
+    git checkout master
+    git checkout -B $subfamilyName-vf
+
+
+    # -------------------------------------------------------------------
+    # move fonts --------------------------------------------------------
+    mkdir -p ofl/$(basename $subfamilyDir)
+    # copy font in split_vf to top
+
+    splitVF=$(ls $encodeDir/$subfamilyDir/split_vf/*.ttf) # should only be one
+    cp $splitVF ofl/$subfamilyName/$(basename $splitVF)
+
+    # copy in statics to "static" folder
+
+
+    # push to upstream branch to make PR simple
+    # TODO: ¿Make a PR from the command line?
+
+    # reset as needed
+    cd $encodeDir
 done
+
