7f94ac4bcc9f568c39576f3632ded398dafbd929
Stephen Nixon <swr410@gmail.com>
2019-04-25 13:24:21 -0400

    complete pr-to-test script

--- docs/16-autohint-settings-test/PR-to-test.sh
+++ docs/16-autohint-settings-test/PR-to-test.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 # This script makes separate PRs to m4rc1e/fonts to trigger diffenator tests of Encode with different autohint settings
-# 
-# REQUIRES: https://github.com/github/hub to make PRs
 #
 # USAGE: 
 # call this script from the root of your encode repo:
 # $ chmod +x docs/16-autohint-settings-test/PR-to-test.sh
 # $ docs/16-autohint-settings-test/PR-to-test.sh
-
-
 
 # for file in hinted folder
 #   make m4rc1e/fonts branch labeled with autohint settings
@@ -18,6 +14,7 @@
 set -ex
 source venv/bin/activate
 
+encodeDir=$(pwd)
 fontDir=$(cd docs/16-autohint-settings-test/hintedFonts && pwd)
 testFonts=$(ls docs/16-autohint-settings-test/hintedFonts)
 marcFontsDir="/Users/stephennixon/type-repos/google-font-repos/m4rc1e-fonts"
@@ -61,11 +58,13 @@ for ttf in $testFonts; do
     # commit files ---------------------------
 
     git add .
-    git commit -m "add autohint test VF for ofl/$subfamilyName"
+    git commit -m "autohint test: $testName"
 
     git push --force --set-upstream origin $testName 
 
-    hub pull-request -m "autohint test VF for ofl/$subfamilyName"
+    # NEVERMIND, this requires login each time over HTTPS
+    # https://github.com/github/hub/issues/2105
+    # hub pull-request -m "autohint test VF for ofl/$subfamilyName"
 
     # reset as needed
     cd $encodeDir
